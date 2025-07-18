41ccda3e1a55767fdafea265cfca880311c64d82
Eric Cheung <ericcheun251@gmail.com>
2025-07-17 23:45:46 -0700

    wip: polishing race fetch functions

--- src/race-weekends/fetch-races.py
+++ src/race-weekends/fetch-races.py
@@ -12,9 +12,10 @@ season_calender = os.path.join(project_root, "data/season-calendar")
 def parse_number_of_rounds():
     """
     Tells you the number of rounds from a particular F1 season using schedule csv.
+    Returns a list of dictionaries with year as key and a list of rounds as keys.
     """
     season_schedules = glob.glob(os.path.join(season_calender, "*.csv"))
-    print(season_schedules)
+    # print(season_schedules)
     season_dicts = []
 
     for file in season_schedules:
@@ -29,11 +30,18 @@ def parse_number_of_rounds():
     
     return season_dicts
 
-# event1 = fastf1.get_event(2024, 1)
-# race1 = event1.get_race()
-# race1.load()
-# print(race1.results)
-# race1.results.to_csv("race_results.csv", index=False)
+def get_race_results(championship_year, round_number):
+    """
+    Gives race results from taking a year and round number.
+    Returns a pandas dataframe of the race results.
+    """
+    event = fastf1.get_event(championship_year, round_number)
+    race_session = event.get_race()
+    race_session.load()
+    # print(race_session.results)
+    
+    return race_session.results
 
 if __name__ == "__main__":
-    print(parse_number_of_rounds())
+#     print(parse_number_of_rounds())
+    print(get_race_results(2024, 1))
